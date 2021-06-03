c6084632107644b474e83b0bf85e158871273bde
GursharanSingh <gursharansinghsandhu352@gmail.com>
2021-06-03 16:48:06 +0530

    asdfas

--- oop_assign1.cpp
+++ oop_assign1.cpp
@@ -7,7 +7,6 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Hello World From Gursharan!\n";
-    std::cout << "Hello World From Gursharan2!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
