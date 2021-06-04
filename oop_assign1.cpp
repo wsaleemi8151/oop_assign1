a1f79bdecbf05d2d89f0f7718e1ea25f577c8e60
GursharanSingh <gursharansinghsandhu352@gmail.com>
2021-06-04 23:54:31 +0530

    Addition of overload functions / check range function by Gursharan

--- oop_assign1.cpp
+++ oop_assign1.cpp
@@ -3,6 +3,30 @@
 
 #include <iostream>
 
+
+
+
+/*Addition by G------------------- */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#pragma warning (disable : 4996)
+
+/* function prototypes */
+int checkRange(int value, int mini_value, int max_value);
+void outputGrade(double numGrade);
+
+void assessGrade(char* alphaGrade);
+void assessGrade(double numGrade);
+void assessGrade(int num[]);
+
+/*---------------------Addition end G */
+
+
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
@@ -11,7 +35,6 @@ int main()
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
@@ -19,3 +42,203 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+
+
+/*Addition by G------------------- */
+
+/* Overload function1 - Take only 1 parameter : [char *] - for letter grade/special situation. */
+void assessGrade(char* alphaGrade)
+{
+	char letterGrade[6];
+	double numGrade = 0;
+	int i = 0;
+	strcpy(letterGrade, alphaGrade);
+
+	if (strcmp(letterGrade, "A+") == 0)
+	{
+		numGrade = 95;
+	}
+	else
+	{
+		if (strcmp(letterGrade, "A") == 0)
+		{
+			numGrade = 85;
+		}
+		else
+		{
+			if (strcmp(letterGrade, "B+") == 0)
+			{
+				numGrade = 77;
+			}
+			else
+			{
+				if (strcmp(letterGrade, "B") == 0)
+				{
+					numGrade = 72;
+				}
+				else
+				{
+					if (strcmp(letterGrade, "C+") == 0)
+					{
+						numGrade = 67;
+					}
+					else
+					{
+						if (strcmp(letterGrade, "C") == 0)
+						{
+							numGrade = 62;
+						}
+						else
+						{
+							if (strcmp(letterGrade, "D") == 0)
+							{
+								numGrade = 57;
+							}
+							else
+							{
+								if (strcmp(letterGrade, "F") == 0)
+								{
+									numGrade = 50;
+								}
+								else
+								{
+									if (strcmp(letterGrade, "I") == 0)
+									{
+										i = -1;
+										printf("Student has Special Situation : I (Incomplete)\n");
+									}
+									else
+									{
+										if (strcmp(letterGrade, "Q") == 0)
+										{
+											i = -1;
+											printf("Student has Special Situation : Q (Withdrawal After Drop/Refund Date)\n");
+										}
+										else
+										{
+											if (strcmp(letterGrade, "AU") == 0)
+											{
+												i = -1;
+												printf("Student has Special Situation : AU (Audit Condition)\n");
+											}
+											else
+											{
+												if (strcmp(letterGrade, "DNA") == 0)
+												{
+													i = -1;
+													printf("Student has Special Situation : DNA (Did Not Attend)\n");
+												}
+												else
+												{
+													if (strcmp(letterGrade, "I/P") == 0)
+													{
+														i = -1;
+														printf("Student has Special Situation : I/P (In Process)\n");
+													}
+													else
+													{
+														numGrade = -1;
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if (i != -1)
+	{
+		assessGrade(numGrade);
+	}
+
+}
+
+
+/* Overload function2 - take single parameter : [double] - for final mark.*/
+void assessGrade(double numGrade)
+{
+	if ((numGrade >= 0.0) && (numGrade <= 100))
+	{
+		outputGrade(numGrade);
+	}
+	else
+	{
+		printf("**ERROR : Invalid Input\n");
+	}
+}
+
+
+/* Overload function3 - take [ int [5] ] as array - indvidual assignment marks. */
+void assessGrade(int num[])
+{
+	double finalGrade = 0;
+	int returnVal = 1;
+	int sum = 0;
+	int i = 0;
+
+	while (i < 5)
+	{
+		returnVal = checkRange(num[i], 0, 100);
+		if (returnVal == 0)
+		{
+			printf("**ERROR : Invalid Input\n");
+			break;
+		}
+		sum += num[i];
+		i++;
+	}
+
+	finalGrade = sum / 5;
+
+	if (returnVal != 0)
+	{
+		outputGrade(finalGrade);
+	}
+}
+
+
+/* Function used to avoid code duplication- for output from overload functions*/
+void outputGrade(double numGrade)
+{
+	if (numGrade >= 54.50)
+	{
+		printf("Student achieved %.2f %% which is a PASS condition.\n", numGrade);
+	}
+	else
+	{
+		printf("Student achieved %.2f %% which is a FAIL condition.\n", numGrade);
+	}
+}
+
+
+
+/*
+ * Function: checkRange()
+ * Parameters: int value, int mini_value, int max_value, where value is the numerical amount to check for validity.
+ * Returns: It return 0 if the value is out of range and 1 if the value is within the specified range.
+ * Description: This function returns different values, indicating a value to be in or out of range.
+*/
+int checkRange(int value, int mini_value, int max_value)
+{
+	/* Using if-esle statement with logical operation [And (&&)] to check if the value lies between the maximum and minimum value. */
+	if ((value >= mini_value) && (value <= max_value))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+/*----------------------Addition end G */
